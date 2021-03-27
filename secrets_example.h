29e63c42e163597cab234765796d66b5dd546cd2
gds32571 <gds32571@gmail.com>
2021-03-27 06:57:24 -0400

    Updated secrets_example.h

--- secrets_example.h
+++ secrets_example.h
@@ -1,3 +1,8 @@
 #define SSID            "router name"
 #define SSIDPWD         "your wifi password"
 #define MYADDR          200
+
+#define mqtt_user "mickey"                  //enter your MQTT username
+#define mqtt_password "mouse"               //enter your password
+#define mqtt_client "client-soilmoisture1"  // must be unique for each board/client
+
